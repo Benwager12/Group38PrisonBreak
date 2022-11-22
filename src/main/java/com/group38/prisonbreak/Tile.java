edcf898a9bf116efd2b300aa0ac5e8c9feef91c2
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-22 12:00:44 +0000

    Added hasColours and getColours into Tile class

--- src/main/java/com/group38/prisonbreak/Tile.java
+++ src/main/java/com/group38/prisonbreak/Tile.java
@@ -1,9 +1,15 @@
+/**
+ * A Class that implements a tile
+ * @aurhor Daniel Banks, Ben Wager
+ */
+
 package com.group38.prisonbreak;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Tile implements Drawable {
@@ -17,8 +23,41 @@ public class Tile implements Drawable {
         put(5, Color.rgb(253, 5, 253));
     }};
 
+    // all the colours on the tile
+    private final Color[] colours = new Color[4];
+
+    public Tile(int[] colours) {
+        for (int i = 0; i < 4; i++) {
+            this.colours[i] = colourMap.get(colours[i]);
+        }
+    }
+
     @Override
     public void draw(GraphicsContext g) {
 
     }
+
+    /**
+     * Gets all the colours of the tile
+     * @return Color[]
+     */
+    public Color[] getColours() {
+        return colours;
+    }
+
+    /**
+     * Checks if any of the colours in a given array is on the tile
+     * @param checkColours array of Colors
+     * @return boolean
+     */
+    public boolean hasColours(Color[] checkColours) {
+        for (Color checkColour : checkColours) {
+            if (Arrays.asList(colours).contains(checkColour)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
