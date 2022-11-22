6a1cd77c467bdad50af668bf8523908a9e1133c4
Benwager12 <benwager@live.co.uk>
2022-11-22 12:33:02 +0000

    Annotate drawable with javadocs

--- src/main/java/com/group38/prisonbreak/Drawable.java
+++ src/main/java/com/group38/prisonbreak/Drawable.java
@@ -2,10 +2,22 @@ package com.group38.prisonbreak;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * An interface that everything that is drawable onto the canvas will implement.
+ * @author Ben Wager (2108500)
+ */
+
 public interface Drawable {
 
+    /**
+     * Draw the item
+     * @param g The graphics context
+     */
     void draw(GraphicsContext g);
 
+    /** The X coordinate */
     int X = 0;
+
+    /** The Y coordinate */
     int Y = 0;
 }
