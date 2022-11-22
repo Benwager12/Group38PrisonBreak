fb9390eccee69e8f581ae23e5d5badc71d7c71dd
Benwager12 <benwager@live.co.uk>
2022-11-22 12:33:39 +0000

    Made a singleton game manager class where everything about the level will be stored

---
+++ src/main/java/com/group38/prisonbreak/GameManager.java
@@ -0,0 +1,12 @@
+package com.group38.prisonbreak;
+
+/**
+ * A singleton class that holds everything in the level.
+ *
+ * @author Ben Wager (2108500)
+ */
+public class GameManager {
+
+    public static Entity[] entites;
+    public static Tile[][] tiles;
+}
