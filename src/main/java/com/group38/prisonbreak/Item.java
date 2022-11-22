513a329e1b987ed961d44727b5bef84de1e95684
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-22 12:30:46 +0000

    Created Item class

---
+++ src/main/java/com/group38/prisonbreak/Item.java
@@ -0,0 +1,14 @@
+/**
+ *  A class that implements an Item
+ * @author Daniel Banks (2107922)
+ */
+
+package com.group38.prisonbreak;
+
+public abstract class Item {
+
+    /**
+     * defines what happens when a player interacts with an Item
+     */
+    public abstract void interact();
+}
