3fbaaa04f5bc256c71e8a67c2a28a74eeb221536
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-24 10:39:21 +0000

    Added blank draw methods and canMove methods to Level class

--- src/main/java/com/group38/prisonbreak/Level.java
+++ src/main/java/com/group38/prisonbreak/Level.java
@@ -19,12 +19,42 @@ public class Level {
     // All the entities in the Level
     private final Entity[] entities;
 
+    /**
+     * Creates a Level instance
+     * @param levelNumber Level number
+     * @param tiles 2D array of Tiles that make up the level
+     * @param entities All the entities that are on the level
+     */
     public Level(int levelNumber,Tile[][] tiles, Entity[] entities) {
         this.levelNumber = levelNumber;
         this.tiles = tiles;
         this.entities = entities;
     }
 
+    /**
+     * Draws all the Tiles onto the level
+     */
+    private void drawTiles() {
 
+    }
+
+    /**
+     * Draws all Entities onto the level
+     */
+    private void drawEntities(){
+
+    }
+
+    /**
+     * Checks if a move is valid
+     * @param direction The direction the entity wishes to move
+     * @param posX X position of the entity
+     * @param posY Y position of the entity
+     * @return boolean - If the move is valid
+     */
+    public boolean canMove(int direction, int posX, int posY) {
+        // TODO: implement can move
+        return true;
+    }
 
 }
