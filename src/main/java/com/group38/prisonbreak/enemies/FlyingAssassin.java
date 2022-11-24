048cc2c532fce7e1efddffd8cbb079289771827f
mattsalt123 <93153126+mattsalt123@users.noreply.github.com>
2022-11-24 12:45:51 +0000

    Implement Enemy classes

---
+++ src/main/java/com/group38/prisonbreak/enemies/FlyingAssassin.java
@@ -0,0 +1,11 @@
+package com.group38.prisonbreak.enemies;
+
+import com.group38.prisonbreak.utilities.Entity;
+
+public class FlyingAssassin extends Entity {
+
+    public FlyingAssassin(int xPos, int yPos, int direction) {
+        super(xPos, yPos, direction);
+    }
+
+}
