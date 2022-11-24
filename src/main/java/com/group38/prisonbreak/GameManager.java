1d603b50f03be1d1aeb7ceb8a5a84bb15e7d1579
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-24 13:29:50 +0000

    Added ticks to GameManager

--- src/main/java/com/group38/prisonbreak/GameManager.java
+++ src/main/java/com/group38/prisonbreak/GameManager.java
@@ -1,7 +1,12 @@
 package com.group38.prisonbreak;
 
+import com.group38.prisonbreak.enemies.SmartThief;
 import com.group38.prisonbreak.utilities.Entity;
 import com.group38.prisonbreak.utilities.Tile;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 /**
  * A singleton class that holds everything in the level.
@@ -10,8 +15,33 @@ import com.group38.prisonbreak.utilities.Tile;
  */
 public class GameManager {
 
-    public static Entity[] entites;
-    public static Tile[][] tiles;
+    public static Level level;
+
+    public static int money;
 
     public static int time;
+
+    private static final Timeline entityTimeLine = new Timeline(new KeyFrame(Duration.millis(500), event -> moveEntities()));
+    private static final Timeline smartThiefTimeLine = new Timeline(new KeyFrame(Duration.millis(1250), event -> moveSmartThief()));
+
+    public static void startTimelines() {
+        entityTimeLine.setCycleCount(Animation.INDEFINITE);
+        smartThiefTimeLine.setCycleCount(Animation.INDEFINITE);
+    }
+
+    private static void moveEntities() {
+        for (Entity entity : level.getEntities()) {
+            if (!(entity instanceof SmartThief)) {
+                entity.move();
+            }
+        }
+    }
+
+    private static void moveSmartThief() {
+        for (Entity entity : level.getEntities()) {
+            if (entity instanceof SmartThief) {
+                entity.move();
+            }
+        }
+    }
 }
