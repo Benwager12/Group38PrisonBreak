24abcd7dc0009f6596859ecfa0004f89abfa2cbf
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-24 12:32:16 +0000

    Removed useless import

--- src/main/java/com/group38/prisonbreak/utilities/Tile.java
+++ src/main/java/com/group38/prisonbreak/utilities/Tile.java
@@ -4,7 +4,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 
