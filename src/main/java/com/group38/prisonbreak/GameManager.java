e85421ee47f61d0412b3471e6af6f96902b7de49
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-24 13:48:14 +0000

    Added Javadoc

--- src/main/java/com/group38/prisonbreak/GameManager.java
+++ src/main/java/com/group38/prisonbreak/GameManager.java
@@ -2,7 +2,6 @@ package com.group38.prisonbreak;
 
 import com.group38.prisonbreak.enemies.SmartThief;
 import com.group38.prisonbreak.utilities.Entity;
-import com.group38.prisonbreak.utilities.Tile;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -15,17 +14,30 @@ import javafx.util.Duration;
  */
 public class GameManager {
 
+    // Instance of the level that's currently open
     public static Level level;
 
+    // Amount of money the player has picked up
     public static int money;
 
+    // Time left in the game
     public static int time;
 
-    private static final Timeline entityTimeLine = new Timeline(new KeyFrame(Duration.millis(500), event -> moveEntities()));
-    private static final Timeline smartThiefTimeLine = new Timeline(new KeyFrame(Duration.millis(1250), event -> moveSmartThief()));
-    private static final Timeline timeTimeLine = new Timeline(new KeyFrame(Duration.millis(1000), event -> changeTime()));
+    // Tick timeline for the entities to move every 500 milliseconds
+    private static final Timeline entityTimeLine =
+            new Timeline(new KeyFrame(Duration.millis(500), event -> moveEntities()));
 
+    // Tick timeline for the smartThief to move every 12500 milliseconds
+    private static final Timeline smartThiefTimeLine =
+            new Timeline(new KeyFrame(Duration.millis(1250), event -> moveSmartThief()));
 
+    // Tick timeline for the main game clock, updates every 1000 milliseconds (1 second)
+    private static final Timeline timeTimeLine =
+            new Timeline(new KeyFrame(Duration.millis(1000), event -> changeTime()));
+
+    /**
+     * Sets all the cycles for the tick timelines
+     */
     public static void startTimelines() {
         entityTimeLine.setCycleCount(Animation.INDEFINITE);
         smartThiefTimeLine.setCycleCount(Animation.INDEFINITE);
@@ -33,6 +45,9 @@ public class GameManager {
 
     }
 
+    /**
+     * moves all the entities apart from smart thief
+     */
     private static void moveEntities() {
         for (Entity entity : level.getEntities()) {
             if (!(entity instanceof SmartThief)) {
@@ -41,6 +56,9 @@ public class GameManager {
         }
     }
 
+    /**
+     *  moves the smart thief
+     */
     private static void moveSmartThief() {
         for (Entity entity : level.getEntities()) {
             if (entity instanceof SmartThief) {
@@ -49,7 +67,15 @@ public class GameManager {
         }
     }
 
+    /**
+     * changes the main game timer
+     */
     private static void changeTime() {
         // TODO: add timer
+        time--;
+
+        if (time <= 0) {
+            // GAME OVER
+        }
     }
 }
