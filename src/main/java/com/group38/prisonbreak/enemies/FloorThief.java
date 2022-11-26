54b464b8b46f2c33d927f5c4243b4ce758fbcdd7
IssaAlsiyabi <118276423+IssaAlsiyabi@users.noreply.github.com>
2022-11-26 19:03:10 +0000

    test github

--- src/main/java/com/group38/prisonbreak/enemies/FloorThief.java
+++ src/main/java/com/group38/prisonbreak/enemies/FloorThief.java
@@ -7,4 +7,11 @@ public class FloorThief extends Enemy {
     public FloorThief(int xPos, int yPos, int direction) {
         super(xPos, yPos, direction);
     }
+
+    public void doNothing() {
+        int x = 10;
+        for (int i = 0; i < x; i++) {
+            System.out.println("Helllllllllloooooooooo");
+        }
+    }
 }
