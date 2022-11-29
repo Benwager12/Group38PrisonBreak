d4ed6ff979c7e241bb083b4a330e4995061c0ef0
Maisha Begum Chowdhury <92822155+Mai-sha8@users.noreply.github.com>
2022-11-29 22:03:29 +0000

    Set up menu controller class
    in the middle of working on button 'movement'

---
+++ src/main/java/com/group38/prisonbreak/controllers/MenuController.java
@@ -0,0 +1,28 @@
+package com.group38.prisonbreak.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * MenuController handles the...[add]
+ */
+public class MenuController {
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button loadGameButton;
+
+    @FXML
+    private Button exitButton;
+
+    /**
+     * [add]
+     * @param mouseHovers
+     */
+    public void animateButton(MouseEvent mouseHovers) {
+
+    }
+}
