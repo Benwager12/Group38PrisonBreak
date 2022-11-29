80d3e785fb2ed070e8264bc315be830ac56e0f47
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-11-29 21:02:22 +0000

    Fixed recursive bug in FlyingAssassin so the entity will now move correctly

--- src/main/java/com/group38/prisonbreak/enemies/FlyingAssassin.java
+++ src/main/java/com/group38/prisonbreak/enemies/FlyingAssassin.java
@@ -3,6 +3,8 @@ package com.group38.prisonbreak.enemies;
 import com.group38.prisonbreak.GameManager;
 import com.group38.prisonbreak.utilities.Enemy;
 
+import java.text.NumberFormat;
+
 /**
  * Implements an Enemy in the game
  * @author Daniel Banks (2107922)
@@ -15,34 +17,35 @@ public class FlyingAssassin extends Enemy {
 
     @Override
     public void move() {
-        int direction = super.getDirection();
-        // Checks if direction is Up/Down (X)
-        boolean isX = direction == 1 || direction == 3;
+        boolean canMove = false;
+        while (!canMove) {
+            int direction = super.getDirection();
 
-        // Checks if direction is negative (Up/Left)
-        boolean isNegative = direction == 0 || direction == 3;
+            boolean isX = direction == 1 || direction == 3;
+            boolean isNegative = direction == 0 || direction == 3;
 
-        // X Position of the next Tile (Based on Direction)
-        int newX = isX ?  super.getX() + (isNegative ? -1 : 1) : super.getY();
+            // X Position of the next Tile (Based on Direction)
+            int newX = isX ?  super.getX() + (isNegative ? -1 : 1) : super.getY();
 
-        // Y Position of the next Tile (Based on Direction)
-        int newY = !isX ? super.getY() + (isNegative ? -1 : 1) : super.getY();
+            // Y Position of the next Tile (Based on Direction)
+            int newY = !isX ? super.getY() + (isNegative ? -1 : 1) : super.getY();
 
-        if (GameManager.level.canMove(newX, newY, direction, false)) {
-            // sets the new X and Y positions
-            super.setX(newX);
-            super.setY(newY);
-        } else {
-            // Switches direction
-            int newDir;
-            if (isX) {
-                newDir = direction == 1 ? 3 : 1;
-                super.setDirection(newDir);
+            if (GameManager.level.canMove(newX, newY, direction, false)) {
+                // sets the new X and Y positions
+                super.setX(newX);
+                super.setY(newY);
+                canMove = true;
             } else {
-                newDir = direction == 0 ? 2 : 0;
-                super.setDirection(newDir);
+                // Switches direction
+                int newDir;
+                if (isX) {
+                    newDir = direction == 1 ? 3 : 1;
+                    super.setDirection(newDir);
+                } else {
+                    newDir = direction == 0 ? 2 : 0;
+                    super.setDirection(newDir);
+                }
             }
-            move();
         }
     }
 }
