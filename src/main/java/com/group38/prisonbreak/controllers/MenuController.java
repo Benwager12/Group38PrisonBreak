fb97eed84d1c5ea5414c10f905fd11b0f6187906
Maisha Begum Chowdhury <92822155+Mai-sha8@users.noreply.github.com>
2022-12-01 11:45:04 +0000

    Working on button 'animation'
    first figuring out how to make one button move and will then apply this to the rest

--- src/main/java/com/group38/prisonbreak/controllers/MenuController.java
+++ src/main/java/com/group38/prisonbreak/controllers/MenuController.java
@@ -1,7 +1,9 @@
 package com.group38.prisonbreak.controllers;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -13,16 +15,41 @@ public class MenuController {
     private Button newGameButton;
 
     @FXML
+    private ImageView newGameImage;
+
+    @FXML
     private Button loadGameButton;
 
     @FXML
+    private ImageView loadGameImage;
+
+    @FXML
     private Button exitButton;
+
+    @FXML
+    private ImageView exitImage;
 
     /**
      * Adds movement to a button when it is hovered over e.g. during menu selection
      * @param e The interaction of the mouse with the button.
      */
     public void animateButton(MouseEvent e) {
+    ChangeListener<Boolean> mousePresent = (observable, oldValue, newValue) -> {
+    if (e.getTarget() == newGameImage) {
+        double originalPos = newGameImage.getRotate();
 
+        // tilt button whilst it is the target
+        while(e.getTarget() == newGameImage) {
+        newGameImage.setRotate(1);
+        newGameButton.setRotate(1);
+        }
+        // return button to original state
+        newGameImage.setRotate(originalPos);
+        newGameButton.setRotate(originalPos);
+     }
+    };
+    newGameImage.hoverProperty().addListener(mousePresent);
+    loadGameImage.hoverProperty().addListener(mousePresent);
+    exitImage.hoverProperty().addListener(mousePresent);
     }
 }
