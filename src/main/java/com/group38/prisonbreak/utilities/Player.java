92cf3b5d250d828e5333965776ea967752f8ff6e
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-12-01 22:10:41 +0000

    Changed move function in Player so that it moves correctly

--- src/main/java/com/group38/prisonbreak/utilities/Player.java
+++ src/main/java/com/group38/prisonbreak/utilities/Player.java
@@ -17,28 +17,13 @@ public class Player extends Entity {
     public Player(int xPos, int yPos, int direction) {
         super(xPos, yPos, direction);
         setEntityImage("playerDog");
-        System.out.printf("%d, %d%n", xPos, yPos);
     }
 
     @Override
     public void move() {
-        boolean canMove = false;
-            int direction = super.getDirection();
-
-            boolean isX = direction == 1 || direction == 3;
-            boolean isNegative = direction == 0 || direction == 3;
-
-            // X Position of the next Tile (Based on Direction)
-            int newX = isX ?  super.getX() + (isNegative ? -1 : 1) : super.getX();
-
-            // Y Position of the next Tile (Based on Direction)
-            int newY = !isX ? super.getY() + (isNegative ? -1 : 1) : super.getY();
-
-            if (GameManager.level.canMove(newX, newY, direction, true)) {
-                // sets the new X and Y positions
-                super.setX(newX);
-                super.setY(newY);
-        }
+        int[] newPos = GameManager.level.moveTo(super.getX(), super.getY(), super.getDirection());
+        super.setX(newPos[0]);
+        super.setY(newPos[1]);
         itemInteract();
     }
 
