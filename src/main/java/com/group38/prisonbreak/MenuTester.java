580eb38d691f0f3ff0370f2af9c857d0336f47aa
Maisha Begum Chowdhury <92822155+Mai-sha8@users.noreply.github.com>
2022-12-01 12:21:34 +0000

    Updated MenuTester
    still not working

--- src/main/java/com/group38/prisonbreak/MenuTester.java
+++ src/main/java/com/group38/prisonbreak/MenuTester.java
@@ -1,5 +1,32 @@
 package com.group38.prisonbreak;
-// 
+// delete after - just for testing
+
+import com.group38.prisonbreak.utilities.FileUtilities;
 
 public class MenuTester extends Application {
+    // scene dimensions
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 446;
+
+    public void start(Stage primaryStage) {
+        FileUtilities.setGameInstance(this);
+        FXMLLoader loader = new FXMLLoader(FileUtilities.getResource("fxml/start-menu.fxml"));
+
+        Pane root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setMinWidth(WIDTH);
+        primaryStage.setMinHeight(HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setTitle("Menu Test");
+    }
+
+    public static void main(String[] args) { launch (args); }
 }
