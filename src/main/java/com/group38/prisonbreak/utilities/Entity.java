cc4abec2e91aa6e665c6749a909ed5c4c2b68253
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-12-01 11:34:39 +0000

    Added entity image functionality to Entity

--- src/main/java/com/group38/prisonbreak/utilities/Entity.java
+++ src/main/java/com/group38/prisonbreak/utilities/Entity.java
@@ -6,8 +6,12 @@
 package com.group38.prisonbreak.utilities;
 
 import com.group38.prisonbreak.GameManager;
+import javafx.scene.image.Image;
 
 public abstract class Entity {
+
+    // Path of the entity images
+    private static final String IMAGE_URL = "images/GameImages/%s.png";
 
     // X position of the entity
     private int x;
@@ -17,6 +21,8 @@ public abstract class Entity {
 
     // Direction the entity is facing/moving
     private int direction;
+
+    private Image entityImage;
 
     public Entity(int xPos, int yPos, int direction) {
         x = xPos;
@@ -56,9 +62,13 @@ public abstract class Entity {
         this.direction = direction;
     }
 
+    public Image getEntityImage() {
+        return entityImage;
+    }
+
     /**
      * sets the X position
-     * @param newX
+     * @param newX X position
      */
     protected void setX(int newX) {
         x = newX;
@@ -66,7 +76,7 @@ public abstract class Entity {
 
     /**
      * sets the Y position
-     * @param newY
+     * @param newY Y position
      */
     protected void setY(int newY) {
         y = newY;
@@ -86,5 +96,13 @@ public abstract class Entity {
      * Interacts with an item that's on the current tile
      */
     protected abstract void itemInteract();
+
+    /**
+     * sets EntitySpriteURL
+     * @param entityName Name of the png to be loaded
+     */
+    protected void setEntityImage(String entityName) {
+        entityImage = FileUtilities.loadImageFromResource(String.format(IMAGE_URL, entityName));
+    }
 
 }
