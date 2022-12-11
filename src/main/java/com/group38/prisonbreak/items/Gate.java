3fa20fd450ec33d82f2cc18265a3d50e2b811f2a
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-12-11 18:54:28 +0000

    Formatted code to pass checkstyle

--- src/main/java/com/group38/prisonbreak/items/Gate.java
+++ src/main/java/com/group38/prisonbreak/items/Gate.java
@@ -8,27 +8,31 @@ import javafx.scene.image.Image;
 import java.util.HashMap;
 
 /**
- * Implements a Gate in the game
+ * Implements a Gate in the game.
  * @author Daniel Banks (2107922)
  */
 public class Gate extends Item {
     private final int gateColour;
 
-    private static final HashMap<Integer, String> imagePathCache = new HashMap<>() {{
-        put(0, GAME_IMAGE_PATH + "gate_rusted_locked.png");
-        put(1, GAME_IMAGE_PATH + "gate_bronze_locked.png");
-        put(2, GAME_IMAGE_PATH + "gate_silver_locked.png");
-        put(3, GAME_IMAGE_PATH + "gate_gold_locked.png");
+    private static final HashMap<Integer, String> IMAGE_PATH_CACHE =
+            new HashMap<>() {{
+            put(0, GAME_IMAGE_PATH + "gate_rusted_locked.png");
+            put(1, GAME_IMAGE_PATH + "gate_bronze_locked.png");
+            put(2, GAME_IMAGE_PATH + "gate_silver_locked.png");
+            put(3, GAME_IMAGE_PATH + "gate_gold_locked.png");
 
     }};
 
-    private static final HashMap<Integer, Image> imageCache = new HashMap<>();
+    private static final HashMap<Integer, Image> IMAGE_CACHE = new HashMap<>();
 
     public Gate(String metadata) {
         gateColour = Integer.parseInt(metadata);
-        if (imageCache.isEmpty()) {
-            imagePathCache.keySet().forEach(index -> {
-                imageCache.put(index, FileUtilities.loadImageFromResource(imagePathCache.get(index)));
+        if (IMAGE_CACHE.isEmpty()) {
+            IMAGE_PATH_CACHE.keySet().forEach(index -> {
+                IMAGE_CACHE.put(index, FileUtilities.loadImageFromResource(
+                        IMAGE_PATH_CACHE.get(index)
+                        )
+                );
             });
         }
     }
@@ -48,12 +52,12 @@ public class Gate extends Item {
     }
 
     /**
-     * @return An imasge that represents whether a gate is open or closed.
+     * @return An image that represents whether a gate is open or closed.
      */
     @Override
     public Image getImage() {
         boolean isGateOpen = GameManager.getLevel().isGateOpen(this);
-        return imageCache.get(isGateOpen ? null : gateColour);
+        return IMAGE_CACHE.get(isGateOpen ? null : gateColour);
     }
 
     public int getGateColour() {
