d87c153b0a3ef8d34aa602e9be7e9a0c202ce9a2
mattsalt123 <93153126+mattsalt123@users.noreply.github.com>
2022-12-11 09:07:33 +0000

    Last comment added before work

--- src/main/java/com/group38/prisonbreak/controllers/AskOverwriteController.java
+++ src/main/java/com/group38/prisonbreak/controllers/AskOverwriteController.java
@@ -56,6 +56,8 @@ public class AskOverwriteController {
 
     @FXML
     private void yesClicked(MouseEvent actionEvent) {
+        //Maybe add functionality to delete a saved level file???
+
         FileUtilities.getGameInstance().setRoot("loadOverwriteLevel");
     }
 
