ee9746a8bd702fd464390374252ddcee8fae310c
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-12-11 17:48:43 +0000

    Formatted code to pass checkstyle

--- src/main/java/com/group38/prisonbreak/entities/enemies/SmartThief.java
+++ src/main/java/com/group38/prisonbreak/entities/enemies/SmartThief.java
@@ -10,19 +10,23 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Implements a Smart Thief in the game
+ * Implements a Smart Thief in the game.
  * @author Daniel Banks (2107922)
  */
 public class SmartThief extends Enemy {
 
-    // Class to find and search for items on the level
+    /**
+     * Class to find and search for items on the level.
+     */
     private final PathFinding searchFinding = new PathFinding();
 
-    // ArrayList of moves to get to the closest item
+    /**
+     * ArrayList of moves to get to the closest item.
+     */
     private final ArrayList<int[]> positionsToItem = new ArrayList<>();
 
     /**
-     * Creates an instance of Smart Thief
+     * Creates an instance of Smart Thief.
      * @param xPos Starting X position
      * @param yPos Starting Y position
      * @param direction Starting direction
@@ -33,15 +37,16 @@ public class SmartThief extends Enemy {
     }
 
     /**
-     * Gets a random position to move to
+     * Gets a random position to move to.
      * @return Position to move to as an int array [X, Y]
      */
     public int[] getRandomMove() {
         ArrayList<Integer> triedDirections = new ArrayList<>();
-        int[] newPosition = new int[] { super.getX(), super.getY()};
+        int[] newPosition = new int[] {super.getX(), super.getY()};
 
         // Loop until it finds a position it can move to
-        while (newPosition[0] == super.getX() && newPosition[1] == super.getY()) {
+        while (newPosition[0] == super.getX()
+                && newPosition[1] == super.getY()) {
 
             // Gets a random direction id
             Random random = new Random();
@@ -50,7 +55,13 @@ public class SmartThief extends Enemy {
 
             // If it hasn't already tried the direction then get a position
             if (!triedDirections.contains(randomDirection)) {
-                newPosition = GameManager.getLevel().moveTo(super.getX(), super.getY(), randomDirection);
+                newPosition =
+                        GameManager.getLevel()
+                                .moveTo(
+                                        super.getX(),
+                                        super.getY(),
+                                        randomDirection
+                                );
                 triedDirections.add(randomDirection);
             }
         }
@@ -58,7 +69,7 @@ public class SmartThief extends Enemy {
     }
 
     /**
-     * updates and sets the new position of the Smart Thief
+     * updates and sets the new position of the Smart Thief.
      * @param position int array [X, Y] of the new position
      */
     public void updatePosition(int[] position) {
@@ -67,37 +78,51 @@ public class SmartThief extends Enemy {
     }
 
     /**
-     * Checks if the item it's heading towards still exists
+     * Checks if the item it's heading towards still exists.
      * @return boolean if item exists
      */
    private boolean itemExist() {
-        if (positionsToItem.size() == 0) return false;
+        if (positionsToItem.size() == 0) {
+            return false;
+        }
         int itemXPosition = positionsToItem.get(positionsToItem.size() - 1)[0];
         int itemYPosition = positionsToItem.get(positionsToItem.size() - 1)[1];
-        return GameManager.getLevel().getTile(itemXPosition, itemYPosition).getItem() != null;
+        return GameManager.getLevel()
+                .getTile(itemXPosition, itemYPosition).getItem() != null;
    }
 
     /**
-     * Moves the smartThief
+     * Moves the smartThief.
      */
     @Override
     public void move() {
         // If there's positions left to move to then change to the first position in the list of moves
         if (positionsToItem.size() > 0 && itemExist()) {
-            if (GameManager.getLevel().wontCollide(this, positionsToItem.get(0)[0], positionsToItem.get(0)[1])) {
+            if (GameManager.getLevel()
+                    .wontCollide(
+                            this,
+                            positionsToItem.get(0)[0],
+                            positionsToItem.get(0)[1])
+            ) {
                 updatePosition(positionsToItem.get(0));
                 positionsToItem.remove(0);
             }
         } else {
             positionsToItem.clear();
             // Gets the shortest path to an item and adds the list of moves/positions to the Arraylist
-            ArrayList<int[]> path = searchFinding.searchForItems(super.getX(), super.getY());
+            ArrayList<int[]> path =
+                    searchFinding.searchForItems(super.getX(), super.getY());
 
             // If no items/path to items are found then the Smart thief will move randomly
             if (path.size() == 0) {
                 int[] newPosition = getRandomMove();
 
-                if (GameManager.getLevel().wontCollide(this, newPosition[0], newPosition[1])) {
+                if (GameManager.getLevel()
+                        .wontCollide(
+                                this,
+                                newPosition[0],
+                                newPosition[1])
+                ) {
                     updatePosition(newPosition);
                 }
             } else {
@@ -111,7 +136,7 @@ public class SmartThief extends Enemy {
     }
 
     /**
-     * Gets the image of the Smart Thief
+     * Gets the image of the Smart Thief.
      * @return Image fo the Smart Thief
      */
     @Override
