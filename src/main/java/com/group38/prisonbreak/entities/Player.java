9b9544698926810c9c4f676f6eb37e1c7c3ac767
ZonicoGaming <76685562+DBanks93@users.noreply.github.com>
2022-12-11 17:55:12 +0000

    Formatted code to pass checkstyle

--- src/main/java/com/group38/prisonbreak/entities/Player.java
+++ src/main/java/com/group38/prisonbreak/entities/Player.java
@@ -1,5 +1,6 @@
 package com.group38.prisonbreak.entities;
 
+import com.group38.prisonbreak.Constants;
 import com.group38.prisonbreak.GameManager;
 import com.group38.prisonbreak.utilities.Entity;
 import com.group38.prisonbreak.utilities.Item;
@@ -8,57 +9,68 @@ import javafx.scene.input.KeyCode;
 import java.util.HashMap;
 
 /**
- * Implements a Player in the game
+ * Implements a Player in the game.
  * @author Daniel Banks (2107922), Matthew Salter (986488)
  */
 public class Player extends Entity {
 
-    private static final HashMap<Integer, String> imagePathCache = new HashMap<>() {{
-        put(1, "outfit_jumpsuit");
-        put(2, "outfit_38_jumper");
-        put(3, "outfit_tuxedo");
-        put(4, "outfit_tank_top");
-        put(5, "outfit_suit");
-        put(6, "outfit_stripey_fit");
-        put(7, "outfit_guard");
-        put(8, "outfit_xmas");
-    }};
+    private static final HashMap<Integer, String> IMAGE_PATH_CACHE =
+            new HashMap<>() {{
+                put(1, "outfit_jumpsuit");
+                put(2, "outfit_38_jumper");
+                put(3, "outfit_tuxedo");
+                put(4, "outfit_tank_top");
+                put(5, "outfit_suit");
+                put(6, "outfit_stripey_fit");
+                put(7, "outfit_guard");
+                put(8, "outfit_xmas");
+            }};
 
     /**
-     * Creates and instance of player
+     * Creates and instance of player.
      * @param xPos X position of the player
      * @param yPos Y position of the player
      * @param direction starting direction of the player
+     * @param outfitIndex index of the player outfit to be worn
      */
     public Player(int xPos, int yPos, int direction, int outfitIndex) {
         super(xPos, yPos, direction);
-        setEntityImage(imagePathCache.get(outfitIndex));
+        setEntityImage(IMAGE_PATH_CACHE.get(outfitIndex));
     }
 
+    /**
+     * Moves the player based on the keys pressed.
+     * Is called every player tick
+     */
     @Override
     public void move() {
         for (KeyCode c : GameManager.getCurrentlyPressed()) {
             if (c == KeyCode.UP || c == KeyCode.W) {
-                setDirection(0);
+                setDirection(Constants.UP_ID);
                 break;
             } else if (c == KeyCode.RIGHT || c == KeyCode.D) {
-                setDirection(1);
+                setDirection(Constants.RIGHT_ID);
                 break;
             } else if (c == KeyCode.DOWN || c == KeyCode.S) {
-                setDirection(2);
+                setDirection(Constants.DOWN_ID);
                 break;
             } else if (c == KeyCode.LEFT || c == KeyCode.A) {
-                setDirection(3);
+                setDirection(Constants.LEFT_ID);
                 break;
             } else if (c == KeyCode.K) {
-                System.out.println(GameManager.getLevel().isGateOpen(4));
                 break;
             } else if (c == KeyCode.F) {
                 GameManager.saveGame();
             }
         }
 
-        int[] newPos = GameManager.getLevel().moveTo(super.getX(), super.getY(), super.getDirection());
+        int[] newPos =
+                GameManager.getLevel()
+                        .moveTo(
+                                super.getX(),
+                                super.getY(),
+                                super.getDirection()
+                        );
         super.setX(newPos[0]);
         super.setY(newPos[1]);
 
